2e1ff8e9db8543b1f04cff46a3d090be8299d27d
EmmanuelGT <47125653+EmmanuelGardunoTellez@users.noreply.github.com>
2022-06-28 22:11:48 -0500

    EmmanuelGT

---
+++ 3er/POO/graficacion java/saludo/src/main/java/saludo/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saludo;
+
+/**
+ *
+ * @author EMANUEL
+ */
+public class Principal {
+    public static void main(String[] args){
+        Saludo v1 = new Saludo();
+        v1.setVisible(true);
+     
+    }
+            
+}
---
+++ 3er/POO/graficacion java/saludo/src/main/java/saludo/Saludo.java
@@ -0,0 +1,11 @@
+
+package saludo;
+
+import javax.swing.JFrame;
+
+public class Saludo extends JFrame{
+    public Saludo(){
+        this.setSize(500, 500); //establece tamaño
+        
+    }
+}
