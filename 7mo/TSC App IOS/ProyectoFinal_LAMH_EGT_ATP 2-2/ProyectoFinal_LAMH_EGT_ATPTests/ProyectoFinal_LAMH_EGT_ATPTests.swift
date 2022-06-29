954aa40267e9deee4a8002b2da94abf148942ff4
EmmanuelGT <47125653+EmmanuelGardunoTellez@users.noreply.github.com>
2022-06-29 13:20:15 -0500

    Add files via upload

---
+++ 7mo/TSC App IOS/ProyectoFinal_LAMH_EGT_ATP 2-2/ProyectoFinal_LAMH_EGT_ATPTests/ProyectoFinal_LAMH_EGT_ATPTests.swift
@@ -0,0 +1,33 @@
+//
+//  ProyectoFinal_LAMH_EGT_ATPTests.swift
+//  ProyectoFinal_LAMH_EGT_ATPTests
+//
+//  Created by user201238 on 11/16/21.
+//
+
+import XCTest
+@testable import ProyectoFinal_LAMH_EGT_ATP
+
+class ProyectoFinal_LAMH_EGT_ATPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
